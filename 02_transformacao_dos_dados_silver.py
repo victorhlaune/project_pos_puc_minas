d864e8c628766bbb64e04c703ad1dbdb37be0980
Victor Laune <victorh.laune@gmail.com>
2024-01-29 23:43:36 -0300

    atualizacoes

--- 02_transformacao_dos_dados_silver.py
+++ 02_transformacao_dos_dados_silver.py
@@ -3,14 +3,11 @@ from pyspark.sql import functions as f
 from pyspark.sql.types import StringType
 import os
 
-
 spark = SparkSession.builder.appName('silver_creator_dados_mortalidade')\
          .config("spark.driver.memory", "16g")\
          .master('local').getOrCreate()
 
-
 PATH = './data/bronze/dados_mortalidade/'
-
 
 df_base = None
 for i in os.listdir(PATH):
@@ -28,13 +25,9 @@ df = df_base.filter((f.col("CAUSABAS") >= "X700") & (f.col("CAUSABAS") <= "X849"
         .filter("DTNASC IS NOT NULL")\
         .filter("ESTCIV IS NOT NULL")\
         .filter("RACACOR IS NOT NULL")\
-        .filter("ESTCIV != '9'")\
-        .filter("SEXO != '0'")\
-        .filter("ESC != '9'")\
         .withColumnRenamed('CAUSABAS', 'causa_morte')\
         .withColumnRenamed('SEXO', 'sexo')\
         .withColumnRenamed('DTOBITO', 'data_obito')\
-        .withColumnRenamed('OBITOPUERP', 'obito_no_puerperio')\
         .withColumnRenamed('DTNASC', 'data_nasc')\
         .withColumnRenamed('ESC', 'escolaridade')\
         .withColumnRenamed('RACACOR', 'raca_cor')\
--- 03_criacao_dos_arquivos_gold.py
+++ 03_criacao_dos_arquivos_gold.py
@@ -1,4 +1,3 @@
-from dateutil.relativedelta import relativedelta
 import pandas as pd
 import os
 
@@ -9,35 +8,29 @@ df['data_obito'] = pd.to_datetime(df['data_obito'])
 df['data_nasc'] = pd.to_datetime(df['data_nasc'])
 
 # Calcule a idade no momento do óbito em anos
-df['idade_momento_obito'] = df.apply(lambda row: relativedelta(row['data_obito'], row['data_nasc']).years, axis=1)
+df['idade_momento_obito'] = (df['data_obito'] - df['data_nasc']).dt.days // 365
 
 df = df.drop('data_nasc', axis=1)
 
 map_sexo = {'1':'masculino', '2':'feminino'}
-map_escolaridade = {'1':'nenhuma', '2':'1 a 3 anos', '3':'4 a 7 anos', '4':'8 a 11 anos', '5':'12 anos e mais'}
 map_raca_cor = {'1':'branca', '2':'preta', '3':'amarela', '4':'parda', '5':'indigena'}
 map_estado_civ = {'1':'solteiro', '2':'casado', '3':'viuvo', '4':'separado judicialmente', '5':'uniao consensual'}
+map_escolaridade = {'1':'nenhuma', '2':'1 a 3 anos', '3':'4 a 7 anos', '4':'8 a 11 anos', '5':'12 anos e mais', '9':'Ignorado'}
+
 
 df['sexo'] = df['sexo'].map(map_sexo)
-df['escolaridade'] = df['escolaridade'].map(map_escolaridade)
 df['raca_cor'] = df['raca_cor'].map(map_raca_cor)
 df['estado_civil'] = df['estado_civil'].map(map_estado_civ)
+df['escolaridade'] = df['escolaridade'].map(map_estado_civ)
 
-df = df.dropna(axis=0)
 
 df_sufoc = df[df['causa_morte'].between('X700', 'X709')]
-df_armas = df[df['causa_morte'].between('X720', 'X749')]
-df_perfuro = df[df['causa_morte'].between('X780', 'X799')]
 
 URL_OUTPUT = './data/gold/'
 
 sufoc_output = 'suicidio_sufoc.csv'
-armas_output = 'suicidio_armas.csv'
-perfuro_output = 'suicidio_perfuro.csv'
 
 if not os.path.exists(URL_OUTPUT):
     os.mkdir(URL_OUTPUT)
 
 df_sufoc.to_csv(os.path.join(URL_OUTPUT, sufoc_output), index=False)
-df_armas.to_csv(os.path.join(URL_OUTPUT, armas_output), index=False)
-df_perfuro.to_csv(os.path.join(URL_OUTPUT, perfuro_output), index=False)
